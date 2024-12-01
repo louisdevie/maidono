ab84841225ea0666ddcfc4b173a9d7cb0bcc4b1d
Louis DEVIE <louisdevie.contact@gmail.com>
2024-12-01 17:35:16 +0100

    Update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -5,7 +5,9 @@ cd maidono-quick-install
 curl -L -o maidono.tgz https://github.com/louisdevie/maidono/releases/download/v0.1.0/maidono-0.1.0-x86_64-unknown-linux-gnu.tar.gz
 tar -xzf maidono.tgz
 chmod u+x install.sh
+set +e
 . install.sh
+set -e
 cd ..
 rm -r maidono-quick-install
 
